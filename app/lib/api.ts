0673d7de68c267e3e4885e7a653e3c77d9e42adb
spidernestdev <contactspidernestdev@gmail.com>
2026-03-21 02:23:55 +0530

    data encrytion

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://expense-tracker-backend-qu69.onrender.com/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://expense-tracker-backend-qu69.onrender.com/api";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
